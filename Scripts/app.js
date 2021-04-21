6fbce5fcf01d2cbc1ec636d3a69a8b35eaa34bec
Darko Goluza <darkogoluza@gmail.com>
2021-04-21 22:26:57 +0200

    Added filtering
    Filter options: All(Shows all items), Done(Shows only done items), Not Done(Shows only items that are not done)

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,42 @@
+class SelectBox {
+  constructor(selector, onOption) {
+    this.main = document.querySelector(selector);
+
+    this.optionsContainer = this.main.querySelector(".options-container");
+    this.selectBoxBtn = this.main.querySelector(".selected-btn");
+    this.options = this.main.querySelectorAll(".option");
+    this.onOption = onOption;
+
+    this.selectBoxBtn.addEventListener("click", () => {
+      this.optionsContainer.classList.toggle("active");
+    });
+
+    this.options.forEach((option, index) => {
+      if (index === 0) {
+        this.makeOptionActive(option);
+      }
+      option.addEventListener("click", () => {
+        this.optionsContainer.classList.remove("active");
+
+        this.deactivateOptions();
+        this.makeOptionActive(option);
+        this.onOption();
+      });
+    });
+  }
+
+  deactivateOptions() {
+    this.options.forEach((item) => {
+      item.classList.remove("active-option");
+    });
+  }
+
+  makeOptionActive(option) {
+    option.classList.add("active-option");
+    this.activeOptionId = option.textContent;
+  }
+}
+
 const inputField = document.querySelector("#input-field");
 const renameInputField = document.querySelector("#rename-input");
 
@@ -8,6 +47,30 @@ const renameButton = document.querySelector("#rename-submit");
 const itemHolder = document.querySelector(".items-holder");
 
 const renameScreen = document.querySelector(".rename-item-screen");
+
+const filterBox = new SelectBox("#filter-box", () => {
+  document.querySelectorAll(".todo-item").forEach((item) => {
+    switch (filterBox.activeOptionId) {
+      case "All":
+        item.classList.remove("hide-item");
+        break;
+      case "Not Done":
+        item.classList.contains("item-done")
+          ? item.classList.remove("hide-item")
+          : item.classList.add("hide-item");
+        break;
+      case "Done":
+        item.classList.contains("item-done")
+          ? item.classList.add("hide-item")
+          : item.classList.remove("hide-item");
+        break;
+    }
+  });
+});
+
+const sortbox = new SelectBox("#sort-box", () => {
+  console.log(sortbox.activeOptionId);
+});
 
 let itemToRename = null;
 
@@ -31,6 +94,9 @@ renameButton.addEventListener("click", () => {
 function addItem(content) {
   const item = document.createElement("div");
   item.classList.add("todo-item");
+  if (filterBox.activeOptionId === "Not Done") {
+    item.classList.add("hide-item");
+  }
 
   item.innerHTML = createItemHTML(content);
 
@@ -60,9 +126,17 @@ function removeItemAction(item) {
 function markItemAction(item) {
   item.querySelector(".mark-item-done").addEventListener("click", (e) => {
     e.target.parentElement.classList.toggle("item-done");
-    e.target.parentElement.classList.contains("item-done")
-      ? (e.target.textContent = "Unmark as Done")
-      : (e.target.textContent = "Mark as Done");
+    if (e.target.parentElement.classList.contains("item-done")) {
+      e.target.textContent = "Unmark as Done";
+      if (filterBox.activeOptionId === "Done") {
+        e.target.parentElement.classList.add("hide-item");
+      }
+    } else {
+      e.target.textContent = "Mark as Done";
+      if (filterBox.activeOptionId === "Not Done") {
+        e.target.parentElement.classList.add("hide-item");
+      }
+    }
   });
 }
 
